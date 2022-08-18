009fde0a8b80898cf470ae46bc31daeefdeaae30
xavisson <javipalacio@gmail.com>
2022-08-18 23:15:29 +0200

    trigger ping indifinitely every 10 seconds

--- compass/src/main/java/com/marfeel/compass/core/PingEmitter.kt
+++ compass/src/main/java/com/marfeel/compass/core/PingEmitter.kt
@@ -1,4 +1,25 @@
 package com.marfeel.compass.core
 
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import timber.log.Timber
+
 internal class PingEmitter {
+
+	private val scope = GlobalScope
+	private val pingFrequencyInMs = 10000L
+
+	fun start() {
+		scope.launch {
+			while (true) {
+				ping()
+				delay(pingFrequencyInMs)
+			}
+		}
+	}
+
+	private fun ping() {
+		Timber.d("ping")
+	}
 }
