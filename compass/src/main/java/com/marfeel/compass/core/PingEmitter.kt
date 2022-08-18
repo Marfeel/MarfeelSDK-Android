a71fd42c10a2f9f7cdb0cd2f023dd8856efec602
xavisson <javipalacio@gmail.com>
2022-08-18 23:23:52 +0200

    stop ping

--- compass/src/main/java/com/marfeel/compass/core/PingEmitter.kt
+++ compass/src/main/java/com/marfeel/compass/core/PingEmitter.kt
@@ -1,17 +1,21 @@
 package com.marfeel.compass.core
 
 import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import timber.log.Timber
 
 internal class PingEmitter {
-
-	private val scope = GlobalScope
 	private val pingFrequencyInMs = 10000L
+	private val scope = GlobalScope
+	private var job: Job? = null
+	private var isRunning: Boolean = false
 
 	fun start() {
-		scope.launch {
+		isRunning = true
+
+		job = scope.launch {
 			while (true) {
 				ping()
 				delay(pingFrequencyInMs)
@@ -21,5 +25,11 @@ internal class PingEmitter {
 
 	private fun ping() {
 		Timber.d("ping")
+	}
+
+	fun stop() {
+		job?.cancel()
+		job = null
+		isRunning = false
 	}
 }
