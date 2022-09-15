70fc0d1d80b6edaf30b850a61649fb8cc2287719
xavisson <javipalacio@gmail.com>
2022-09-15 15:46:25 +0200

    [WIP] Post ping using Form

--- compass/src/main/java/com/marfeel/compass/network/ApiClient.kt
+++ compass/src/main/java/com/marfeel/compass/network/ApiClient.kt
@@ -5,6 +5,8 @@ import com.google.gson.Gson
 import com.marfeel.compass.BuildConfig
 import com.marfeel.compass.core.PingData
 import com.marfeel.compass.core.RfvData
+import com.marfeel.compass.core.androidPageType
+import okhttp3.FormBody
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
 import okhttp3.Request
@@ -22,11 +24,19 @@ internal class ApiClient(
 	private val mediaType = "application/json; charset=utf-8".toMediaType()
 
 	fun ping(pingData: PingData) {
-		val jsonRequest = Gson().toJson(pingData)
+		val formBody = FormBody.Builder()
+			.addPingRequest(pingData)
+			.build()
 		val request = Request.Builder()
 			.url("$pingBaseUrl/$pingPath")
-			.post(jsonRequest.toRequestBody(mediaType))
+			.post(formBody)
 			.build()
+
+		val jsonRequest = Gson().toJson(pingData)
+//		val request = Request.Builder()
+//			.url("$pingBaseUrl/$pingPath")
+//			.post(jsonRequest.toRequestBody(mediaType))
+//			.build()
 		httpClient.newCall(request).execute().use {
 			if (it.isSuccessful) Log.d("Compass", "ping emmited")
 		}
@@ -46,3 +56,25 @@ internal class ApiClient(
 		}
 	}
 }
+
+private fun FormBody.Builder.addPingRequest(ping: PingData): FormBody.Builder =
+	this.addEncoded("ac", ping.accountId)
+		.addEncoded("t", ping.sessionTimeStamp.toString())
+		.addEncoded("url", ping.url)
+		.addEncoded("c", ping.url)
+		.addEncoded("pp", ping.previousUrl)
+		.addEncoded("p", ping.pageId)
+		.addEncoded("u", ping.originalUserId)
+		.addEncoded("s", ping.sessionId)
+		.addEncoded("a", ping.pingCounter.toString())
+		.addEncoded("n", ping.currentTimeStamp.toString())
+		.addEncoded("ut", ping.userType.numericValue.toString())
+		.addEncoded("sui", ping.registeredUserId)
+		.addEncoded("sc", ping.scrollPercent.toString())
+		.addEncoded("fv", ping.firsVisitTimeStamp.toString())
+		.addEncoded("lv", ping.previousSessionTimeStamp?.toString() ?: "null")
+		.addEncoded("l", ping.timeOnPage.toString())
+		.addEncoded("ps", ping.pageStartTimeStamp.toString())
+		.addEncoded("conv", ping.conversions ?: "")
+		.addEncoded("v", BuildConfig.VERSION)
+		.addEncoded("pageType", androidPageType.toString())
