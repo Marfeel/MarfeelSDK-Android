ea954327123299aa7a51ae24d07e4b4b40d6571b
Marc <marc.garcia@marfeel.com>
2025-02-25 10:45:30 +0100

    fix: conversions should be tracked individually (#24)

--- app/src/main/java/com/marfeel/demoapp/MainScreen.kt
+++ app/src/main/java/com/marfeel/demoapp/MainScreen.kt
@@ -57,6 +57,7 @@ fun MainScreen(
 	val titleStyle = TextStyle.Default.copy(fontSize = 24.sp, fontWeight = FontWeight.Bold)
 	val coroutineScope = CoroutineScope(Dispatchers.IO)
 
+	tracker.trackScreen("main screen")
 	tracker.setSessionVar("pepe", "pepa")
 	tracker.setSessionVar("pepe2", "pepa2")
 	tracker.setUserVar("lolo", "lola")
--- compass/src/main/java/com/marfeel/compass/core/ping/IngestPingEmitter.kt
+++ compass/src/main/java/com/marfeel/compass/core/ping/IngestPingEmitter.kt
@@ -24,11 +24,11 @@ internal class IngestPingEmitter(
         url: String,
         scrollPosition: Int? = null
     ) {
+        stop()
         lastBackgroundTimeStamp = null
         startBackgroundWatcher()
         pingEmitterState = IngestPingEmitterState(
             url = url,
-            pingCounter = 0,
             scrollPercent = scrollPosition,
             pageStartTimeStamp = currentTimeStampInSeconds(),
             timeOnBackground = 0,
@@ -45,7 +45,6 @@ internal class IngestPingEmitter(
 
     private fun ping() {
         pingEmitterState?.let {
-            pingEmitterState = it.copy(pingCounter = it.pingCounter + 1)
             doPing(it)
         }
     }
@@ -53,6 +52,7 @@ internal class IngestPingEmitter(
     fun stop() {
         stopBackgroundWatcher()
         job.cancelChildren()
+        doPing.resetPing()
         pingEmitterState = null
     }
 
@@ -79,7 +79,6 @@ internal class IngestPingEmitter(
 
 internal data class IngestPingEmitterState(
     val url: String,
-    val pingCounter: Int,
     val scrollPercent: Int?,
     val pageStartTimeStamp: Long,
     val timeOnBackground: Long,
